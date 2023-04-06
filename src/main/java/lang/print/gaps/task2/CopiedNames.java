d7921b8b23cde235df4036e70cced88618bcbd03
Kharlamov <tarantoga90@gmail.com>
2023-04-06 10:46:38 +0400

    task completed

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int firstVariable = 0;
+        int secondVariable = 2000;
+        int thirdVariable = 1000;
+        int fourthVariable = 4000;
+        int fifthVariable = 3000;
+        int sixthVariable = 5000;
+        int seventhVariable = 7000;
+        int lastVariable = 90000;
     }
 }
